0c14e5c195b8c9b6eeaa9dc9416907e730a4200a
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-10 13:44:43 +0300

    Pass down action as props to table

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { SetStateAction, Dispatch } from 'react'
 import Paper from '@mui/material/Paper';
 import MuiTable from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -15,20 +15,33 @@ import { Column, Row } from '../types/data';
 const Table = ({
   columns,
   rows,
+  page,
+  noOfPages,
+  rowsPerPage,
+  setRowsPerPage,
+  setPageNum,
+  sendEmail,
+  activate,
+  confirmPayment
 } : {
   columns: Array<Column>
-  rows: Array<Row>
+  rows: Array<Row>,
+  page: number,
+  noOfPages?: number,
+  rowsPerPage: number,
+  setRowsPerPage: Dispatch<SetStateAction<number>>,
+  setPageNum: Dispatch<SetStateAction<number>>,
+  sendEmail?: (id: string) => void,
+  activate?: (id: string, switchValue: boolean) => void,
+  confirmPayment?: (id: string) => void
 }) => {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
   const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
+    setPageNum(newPage);
   };
 
   const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRowsPerPage(+event.target.value);
-    setPage(0);
+    setPageNum(0);
   };
 
   const chipVariant = (status: string | number | boolean | undefined) => {
@@ -55,18 +68,18 @@ const Table = ({
       case 'total_earnings':
         return column.format && column.format(Number(value))
       case 'ack_payment':
-        return <Button variant='contained' sx={{ fontSize: '0.7rem' }} color='primary' onClick={() => {}}>Confirm Payment</Button>
+        return <Button variant='contained' sx={{ fontSize: '0.7rem' }} color='success' onClick={() =>{ if(confirmPayment) confirmPayment(userId || '') }}>Confirm Payment</Button>
       case 'send_email':
-        return <Button variant='contained' sx={{ fontSize: '0.7rem' }} color='secondary' onClick={() => {}}>Send E-mail</Button>
+        return <Button variant='contained' sx={{ fontSize: '0.7rem' }} color='secondary' onClick={() => { if(sendEmail) sendEmail(userId || '')}}>Send E-mail</Button>
       case 'isDisconnected':
-        return <Switch color='error' onClick={() => {}} value={switchValue} onChange={() => {}} />
+        return <Switch color='error' onClick={() => { if(activate) activate(userId || '', !switchValue) }} value={switchValue} onChange={() => {}} />
       default:
         return value
     }
   }
   
   return (
-    <Paper sx={{ width: '95%', overflow: 'hidden', margin: '1rem auto' }} elevation={8}>
+    <Paper sx={{ width: '95%', overflow: 'hidden', margin: '1rem auto' }} elevation={5}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <MuiTable stickyHeader aria-label="sticky table">
           <TableHead>
@@ -85,7 +98,6 @@ const Table = ({
           </TableHead>
           <TableBody>
             {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
                   <TableRow hover role="checkbox" tabIndex={-1} key={row.name}>
