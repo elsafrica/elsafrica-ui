24662593c4b807ba4cf79b3a76b7ad3e885d751a
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-12 16:54:53 +0300

    Add update functionality on customer listing page

--- src/app/customers/accounts/page.tsx
+++ src/app/customers/accounts/page.tsx
@@ -7,18 +7,16 @@ import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import moment from 'moment';
 import Table from '@/app/components/Table';
-import { AxiosErrorData, Row } from '@/app/types/data';
+import { AxiosErrorData, Column, Row } from '@/app/types/data';
 import axios, { AxiosError } from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import { Notification } from '@/app/types/notification';
-
-interface Column {
-  id: 'name' | 'phone1' | 'phone2' | 'location' | 'ip' | 'created_at' | 'bill' | 'total_earnings' | 'status';
-  label: string;
-  minWidth?: number;
-  align?: 'right' | 'center' | 'left';
-  format?: (value: number) => string;
-}
+import Modal from '@/app/components/Modal';
+import { Form, Formik, FormikHelpers } from 'formik';
+import { object, string } from 'yup';
+import { Button, DialogActions, DialogContent } from '@mui/material';
+import TextField from '@/app/components/TextField';
+import Select from '@/app/components/Select';
 
 const columns: Column[] = [
   { id: 'name', label: 'Customer Name', minWidth: 120 },
@@ -57,9 +55,11 @@ const columns: Column[] = [
     format: (value: number) => value.toFixed(2),
   },
   { id: 'status', label: 'Status', minWidth: 80, align: 'center' },
+  { id: 'actions', label: 'Actions', minWidth: 40, align: 'center' },
 ];
 
 function createData(
+  userId: string,
   name: string,
   phone1: string,
   location: string,
@@ -69,8 +69,11 @@ function createData(
   total_earnings: string,
   status: string,
   phone2?: string,
+  email?: string,
+  package_name?: string,
 ): Row {
   return { 
+    userId,
     name,
     phone1,
     phone2: phone2 || 'N/A',
@@ -80,7 +83,19 @@ function createData(
     bill,
     total_earnings: Number(total_earnings),
     status,
+    email,
+    package_name,
   };
+}
+
+interface FormikValues extends Row {
+  firstName?: string;
+  lastName?: string;
+  phone1?: string;
+  phone2?: string;
+  location?: string;
+  ip?: string;
+  email?: string;
 }
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -88,7 +103,56 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 export default function CustomerAccounts() {
   const [currentPage, setCurrentPage] = useState<number>(0);
   const [rowsPerPage, setRowsPerPage] = useState<number>(10);
-  const [notification, setNotification] = useState<Notification>()
+  const [notification, setNotification] = useState<Notification>();
+  const [update, setUpdate] = useState<FormikValues>();
+
+  const queryClient = useQueryClient();
+
+  const validator = object({
+    firstName: string()
+      .required('Please fill out this field.'),
+    lastName: string()
+      .required('Please fill out this field.'),
+    phone1: string()
+      .required('Please fill out this field.'),
+    phone2: string(),
+    email: string()
+      .email('Please enter a valid e-mail.')
+      .required('Please fill out this field.'),
+    ip: string()
+      .required('Please fill out this field.'),
+    location: string()
+      .required('Please fill out this field.'),
+    package_name: string()
+      .required('Please fill out this field.'),
+  });
+
+  const onUpdateSubmit = async (values: FormikValues, helpers: FormikHelpers<FormikValues>) => {
+    try {
+      const { data } = await axios.patch(`${BASE_URL}/customers/update`, Object.assign(values, { id: values.userId, package: values.package_name }));
+      setNotification({
+        status: 'success',
+        message: data.msg
+      });
+
+      helpers.resetForm();
+      queryClient.invalidateQueries(['customers']);
+    } catch (error: any) {
+      if (error.response) {
+        setNotification({
+          status: 'error',
+          message: error.response.data.msg,
+        });
+      }
+
+      setNotification({
+        status: 'error',
+        message: error.message,
+      });
+    } finally {
+      setUpdate(undefined);
+    }
+  }
 
   const fetchCustomers = async (currentPage: number, rowsPerPage: number) : Promise<{
 		users: Array<any>,
@@ -100,6 +164,14 @@ export default function CustomerAccounts() {
 				rowsPerPage,
 			}
 		})).data;
+		
+		return data;
+	}
+
+  const fetchPackages = async () : Promise<{
+		packages: Array<any>,
+	}> => {
+		 const data = (await axios.get(`${BASE_URL}/packages/get`)).data;
 		
 		return data;
 	}
@@ -121,6 +193,23 @@ export default function CustomerAccounts() {
       });
     },
 	});
+
+  const { data: packages } = useQuery({
+    queryKey: ['packages'],
+    queryFn: () => fetchPackages(),
+    onError: (error: AxiosError<{ msg: string }>) => {
+      if(error.response) {
+        setNotification({
+          status: 'error',
+          message: error?.response.data.msg,
+        });
+      }
+      setNotification({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  })
 
   const checkStatus = (last_payment?: string, isDisconnected?: boolean) : string => {
     const due = 
@@ -148,18 +237,34 @@ export default function CustomerAccounts() {
   }
 
   const rows = data?.users.map((user) => createData(
+    user?._id,
     user?.name, 
     user?.phone1,
     user?.location,
     user?.ip,
     user?.createdAt,
-    user?.bill?.amount,
+    user?.bill?.package,
     user?.total_earnings,
     checkStatus(user?.last_payment, user?.isDisconnected),
     user?.phone2,
+    user?.email,
+    user?.bill?.package,
   )) || [];
 
   const handleNotificationClose = () => setNotification(undefined);
+
+  const onUpdate = (data?: Row) => {
+    const names : string[] = data?.name.split(' ') || ['', ''];
+
+    setUpdate({
+      name: '',
+      firstName: names[0],
+      lastName: names[1],
+      ...data
+    })
+  };
+
+  const onCloseUpdate = () => setUpdate(undefined);
 
   return (
     <>
@@ -186,7 +291,59 @@ export default function CustomerAccounts() {
         setPageNum={setCurrentPage}
         rowsPerPage={rowsPerPage}
         setRowsPerPage={setRowsPerPage}
+        update={onUpdate}
       />
+      <Modal
+        title='Update Customer'
+        open={Boolean(update)}
+        onClose={onCloseUpdate}
+      >
+        <Formik
+          initialValues={update || { name: '' }}
+          validationSchema={validator}
+          onSubmit={onUpdateSubmit}
+          enableReinitialize
+        >
+          {
+            ({ values, errors, getFieldMeta, setFieldValue }) => (
+              <Form>
+                <DialogContent>
+                  <Box
+                    display='flex'
+                    sx={{
+                      flexDirection: { xs: 'column', md: 'row' },
+                      margin: 'auto',
+                      flexWrap: { md: 'wrap' },
+                      justifyContent: { md: 'space-between' }
+                    }}
+                  >
+                    <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="firstName" label='Customer First Name' required />
+                    <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="lastName" label='Customer Last Name' required />
+                    <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="phone1" label='Primary Phone' required />
+                    <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="phone2" label='Secondary Phone' />
+                    <TextField sx={{ width: { md: '100%', lg: '100%' }}} name="email" label='Email' required />
+                    <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="location" label='Location/Apartment' required />
+                    <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="ip" label='IP Address' required />
+                    <Select
+                      sx={{ width: { md: '100%', lg: '100%' }}}
+                      label='Package' 
+                      value={values.package_name || ''} 
+                      values={packages?.packages?.map((item: { name: string, amount: string }) => item.name) || []} 
+                      onChange={(value) => {setFieldValue('package_name', value)}}
+                      isError={Boolean(getFieldMeta('package_name') && errors.package_name)}
+                      error={errors.package_name}
+                    />
+                  </Box>
+                </DialogContent>
+                <DialogActions>
+                  <Button color='error' type='button' onClick={onCloseUpdate}>Cancel</Button>
+                  <Button color='info' type='submit'>Update</Button>
+                </DialogActions>
+              </Form>
+            )
+          }
+        </Formik>
+      </Modal>
       <Snackbar
         autoHideDuration={6000}
         open={Boolean(notification)}
