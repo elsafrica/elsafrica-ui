f330e4ce9469c72a395f1ed99ec24debb179629b
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-15 12:19:47 +0300

    Integrate sign in page with api

--- src/app/sign_in/page.tsx
+++ src/app/sign_in/page.tsx
@@ -1,77 +1,138 @@
 'use client'
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography';
-import { Formik, Form } from 'formik';
+import { Formik, Form, FormikHelpers } from 'formik';
 import TextField, { TextFieldPassword } from '../components/TextField';
 import Button from '@mui/material/Button';
 import { object, string } from 'yup';
+import axios from 'axios';
+import { Alert, Snackbar } from '@mui/material';
+import { Notification } from '../types/notification';
+import { ContextUpdater } from '../providers/context';
+
+interface FormikValues {
+  email: string;
+  password: string;
+}
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const SignIn = () => {
+  const { updateUser, updateAuthToken } = useContext(ContextUpdater);
+  const [notification, setNotification] = useState<Notification>();
+  
   //Form validator
   const validator = object({
-    username: string()
-      .required('Username is required'),
+    email: string()
+      .required('E-mail is required'),
     password: string()
       .required('Password is required'),
   });
 
-  return (
-    <Box
-      display='flex'
-      alignItems='center'
-      justifyContent='center'
-      sx={{
-        width: '100vw',
-        height: '100vh'
-      }}
-    >
-      <Box
-        sx={{
-          maxWidth: { xs: '90%', md: '400px', lg: '400px' },
-          margin: 'auto'
-        }
-      }>
-        <Typography
-          sx={{
-            color: '#91d000',
-            fontSize: '2rem',
-            fontWeight: 'bold',
-            marginBottom: '1.5rem'
-          }}
-          textAlign='center'
-        >
-          Sign In
-        </Typography>
+  const onSubmit = async (values: FormikValues, helpers: FormikHelpers<FormikValues>) => {
+    try {
+      const { data } = await axios.post(`${BASE_URL}/auth/sign_in`, values);
+      setNotification({
+        status: 'success',
+        message: data.msg
+      });
 
-        <Box>
-          <Formik
-            initialValues={{
-              username: '',
-              password: '',
+      updateAuthToken(data.token);
+      updateUser({ email: data.user.email, id: data.user.id, phoneNo: '' });
+
+      helpers.resetForm();
+      } catch (error: any) {
+      if (error.response) {
+        return setNotification({
+          status: 'error',
+          message: error.response.data.err,
+        });
+      }
+
+      setNotification({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  const handleNotificationClose = () => setNotification(undefined);
+
+  return (
+    <>
+      <Box
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+        sx={{
+          width: '100vw',
+          height: '100vh'
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: { xs: '90%', md: '400px', lg: '400px' },
+            margin: 'auto'
+          }
+        }>
+          <Typography
+            sx={{
+              color: '#91d000',
+              fontSize: '2rem',
+              fontWeight: 'bold',
+              marginBottom: '1.5rem'
             }}
-            validationSchema={validator}
-            onSubmit={() => {}}
+            textAlign='center'
           >
-            {
-              () => (
-                <Form>
-                  <Box
-                    display='flex'
-                    flexDirection='column'
-                    paddingX='2rem'
-                  >
-                    <TextField name="username" label='Username' required />
-                    <TextFieldPassword name="password" label='Password' required />
-                    <Button variant='contained' type='submit' sx={{ backgroundColor: '#91d000', color: 'white', margin: '0.25rem 0' }}>Sign In</Button>
-                  </Box>
-                </Form>
-              )
-            }
-          </Formik>
+            Sign In
+          </Typography>
+
+          <Box>
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+              }}
+              validationSchema={validator}
+              onSubmit={onSubmit}
+            >
+              {
+                () => (
+                  <Form>
+                    <Box
+                      display='flex'
+                      flexDirection='column'
+                      paddingX='2rem'
+                    >
+                      <TextField name="email" label='E-mail' required />
+                      <TextFieldPassword name="password" label='Password' required />
+                      <Button variant='contained' type='submit' sx={{ backgroundColor: '#91d000', color: 'white', margin: '0.25rem 0' }}>Sign In</Button>
+                    </Box>
+                  </Form>
+                )
+              }
+            </Formik>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      <Snackbar
+        autoHideDuration={6000}
+        open={Boolean(notification)}
+        onClose={handleNotificationClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+      >
+        <Alert
+          severity={notification?.status}
+          sx={{
+            width: '100%'
+          }}
+          onClose={handleNotificationClose}
+        >
+          {notification?.message}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
