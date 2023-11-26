941ac16c0220cbd7a7e7df222bc6ba93fcb99c15
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-26 16:47:31 +0300

    Edit url for deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    REACT_APP_BASE_URL: 'http://174.138.95.187:8080/api',
-    SOCKET_BASE_URL: 'http://174.138.95.187:8080',
+    REACT_APP_BASE_URL: 'https://174.138.95.187/api',
+    SOCKET_BASE_URL: 'https://174.138.95.187',
   },
   images: {
     remotePatterns: [{
       hostname: '174.138.95.187',
-      protocol: 'http',
-      port: '8080'
+      protocol: 'https',
+      port: ''
     }]
   },
   async redirects() {
