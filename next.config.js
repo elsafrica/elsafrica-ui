76ca92ef173fa003ef81c3d771ef9da8b07e0821
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-26 12:21:35 +0300

    Edit env variable for production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    REACT_APP_BASE_URL: 'http://174.138.95.187:8080',
-    SOCKET_BASE_URL: 'http://http://174.138.95.187:8080',
+    REACT_APP_BASE_URL: 'http://174.138.95.187:8080/api',
+    SOCKET_BASE_URL: 'http://174.138.95.187:8080',
   },
   images: {
     remotePatterns: [{
