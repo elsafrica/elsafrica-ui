cdd2c9d87f17ad132177e65f7352ebb661fc5674
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-26 11:21:26 +0300

    Edit env variable for production Edit Dockerfile and prepare it for production

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    REACT_APP_BASE_URL: 'http://127.0.0.1:8080/api',
-    SOCKET_BASE_URL: '',
+    REACT_APP_BASE_URL: 'http://174.138.95.187:8080',
+    SOCKET_BASE_URL: 'http://http://174.138.95.187:8080',
   },
   images: {
     remotePatterns: [{
-      hostname: '127.0.0.1',
+      hostname: '174.138.95.187',
       protocol: 'http',
       port: '8080'
     }]
