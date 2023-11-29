84bb4905e6fc8bd3d3adb2ee76c64f1ec86ba8fa
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-29 22:51:10 +0300

    Remove disabled when qr is available on request qr button

--- src/app/whatsapp_login/page.tsx
+++ src/app/whatsapp_login/page.tsx
@@ -164,7 +164,7 @@ export default function CustomerAccounts() {
             variant='contained'
             onClick={requestQRCode}
             startIcon={<WhatsAppIcon />}
-            disabled={Boolean(qrCode) || loading}
+            disabled={loading}
           >
             Request QR Code
           </Button>
