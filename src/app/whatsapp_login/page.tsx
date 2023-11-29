f38aaf4aace00fe2fa8de81085015c8226aa6114
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-29 15:36:46 +0300

    Create form for sending whatsapp broadcast message Create a button for testing whatsapp message sending

--- src/app/whatsapp_login/page.tsx
+++ src/app/whatsapp_login/page.tsx
@@ -9,17 +9,25 @@ import { Notification } from '@/app/types/notification';
 import { Context, ContextUpdater } from '../providers/context';
 import QRCode from 'react-qr-code';
 import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import TextArea from '@mui/material/TextareaAutosize';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import CircularProgress from '@mui/material/CircularProgress';
 import AxiosInstance  from '../services/axios';
 import { useAuthorize } from '../helpers/useAuth';
 import Loader from '../components/Loader';
+import { Roboto } from 'next/font/google';
+import { Send } from '@mui/icons-material';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const roboto = Roboto({ weight: '500', subsets: ['latin'] });
 
 export default function CustomerAccounts() {
   const [notification, setNotification] = useState<Notification>();
   const [loading, setLoading] = useState(false);
+  const [isSending, setIsSending] = useState<boolean>();
+  const [message, setMessage] = useState<string>();
 
   const { authToken, qrCode } = useContext(Context);
   const { updateQRCode } = useContext(ContextUpdater);
@@ -54,56 +62,186 @@ export default function CustomerAccounts() {
     }
   }
 
+  const testMessage = async () => {
+    setLoading(true);
+
+    try {
+      const { data } = await axios.get(`${BASE_URL}/messages/send_test_message`);
+
+      setNotification({
+        status: 'success',
+        message: data.msg
+      });
+    } catch (error: any) {
+      if (error.response) {
+        return setNotification({
+          status: 'error',
+          message: error.response.data.errMsg
+        })
+      }
+
+      setNotification({
+        status: 'error',
+        message: error.message
+      })
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const broadcastMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(!message) return setNotification({
+      status: 'error',
+      message: 'Please enter your message the submit.'
+    });
+
+    setIsSending(true);
+
+    try {
+      const { data } = await axios.post(`${BASE_URL}/messages/broadcast_message`, {
+        message
+      });
+
+      setNotification({
+        status: 'success',
+        message: data.msg
+      });
+    } catch (error: any) {
+      if (error.response) {
+        return setNotification({
+          status: 'error',
+          message: error.response.data.errMsg
+        })
+      }
+
+      setNotification({
+        status: 'error',
+        message: error.message
+      })
+    } finally {
+      setIsSending(false);
+    }
+  }
+
+  const onTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+
+    setMessage(value);
+  };
+
   if(!isAuthorized) return <Loader />;
 
   return (
     <>
       <Header />
-      <Box>
-        <Typography
-          sx={{
-            color: '#91d000',
-            fontSize: '1.2rem',
-            fontWeight: 'bold',
-            margin: '1.5rem 1rem 0.5rem',
-          }}
-          component='h1'
-          textAlign='start'
+      <Box
+        sx={{
+          padding: '2rem',
+          display: 'flex',
+          justifyContent: { xs: 'unset', md: 'space-between' },
+          flexDirection: { xs: 'column', md: 'row' },
+          maxWidth: '600px',
+          width: '80%',
+          mx: 'auto'
+        }}
+      >
+        <Box>
+          <Typography
+            sx={{
+              color: '#91d000',
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              margin: '0.5rem 0',
+            }}
+            component='h4'
+            textAlign='start'
         >
           Scan WhatsApp QR Code
         </Typography>
-      </Box>
-      <Box
-        sx={{
-          padding: '2rem'
-        }}
-      >
-        <Button
-          variant='contained'
-          onClick={requestQRCode}
-          startIcon={<WhatsAppIcon />}
-          disabled={!Boolean(qrCode) && loading}
-        >
-          Request QR Code
-        </Button>
-        <Box
-          sx={{
-            maxWidth: '200px',
-            marginTop: '1.5rem'
-          }}
-        >
-          { 
-            typeof qrCode === 'string' && Boolean(qrCode) ?
-            <QRCode
-            size={256}
-            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-            value={qrCode}
-            viewBox={`0 0 256 256`}
-          />
-          : loading ?
-          <CircularProgress />
-          : <></>
-          }
+          <Button
+            variant='contained'
+            onClick={requestQRCode}
+            startIcon={<WhatsAppIcon />}
+            disabled={Boolean(qrCode) || loading}
+          >
+            Request QR Code
+          </Button>
+          <Box
+            sx={{
+              maxWidth: '200px',
+              height: '200px',
+              width: '200px',
+              marginTop: '1.5rem'
+            }}
+          >
+            { 
+              typeof qrCode === 'string' && Boolean(qrCode) ?
+              <QRCode
+              size={200}
+              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+              value={qrCode}
+              viewBox={`0 0 200 200`}
+            />
+            : loading ?
+            <CircularProgress />
+            : <></>
+            }
+          </Box>
+          <Button
+            variant='contained'
+            color='whatsapp'
+            onClick={testMessage}
+            startIcon={<Send />}
+            disabled={Boolean(!qrCode) || loading}
+          >
+            Test Message
+          </Button>
+        </Box>
+        <Box>
+          <Typography
+            sx={{
+              color: '#91d000',
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              margin: '0.5rem 0',
+            }}
+            component='h4'
+            textAlign='start'
+          >
+            Broadcast message
+          </Typography>
+          <Box>
+            <form onSubmit={broadcastMessage}>
+              <Box
+                display='flex'
+                flexDirection='column'
+                justifyContent='start'
+                alignContent='start'
+                alignItems='start'
+              >
+                <FormControl>
+                  <TextArea 
+                    style={{
+                      maxWidth: '250px',
+                      minWidth: '250px',
+                      height: '100px',
+                      minHeight: '50px',
+                      padding: '1rem',
+                      fontSize: '1rem',
+                      letterSpacing: '0.05rem',
+                      clear: 'right'
+                    }}
+                    placeholder='Enter Message'
+                    className={roboto.className}
+                    onChange={onTextAreaChange}
+                  ></TextArea>
+                </FormControl>
+                <Button disabled={isSending} sx={{ mt: '1rem', clear: 'left' }} type='submit' startIcon={<Send />} color='whatsapp' variant='contained'>Send message</Button>
+              </Box>
+            </form>
+          </Box>
         </Box>
       </Box>
       <Snackbar
