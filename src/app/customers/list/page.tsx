673a40ca8e53247aa6d2af0866896a2a88271b81
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-12-04 16:30:28 +0300

    Remove email field from user field

--- src/app/customers/list/page.tsx
+++ src/app/customers/list/page.tsx
@@ -451,7 +451,6 @@ export default function CustomerAccounts() {
                     <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="lastName" label='Customer Last Name' required />
                     <TelTextField value={values.phone1} setFieldValue={setFieldValue} sx={{ width: { md: '48%', lg: '48%' }}} name="phone1" label='Primary Phone' required />
                     <TelTextField value={values.phone2} setFieldValue={setFieldValue} sx={{ width: { md: '48%', lg: '48%' }}} name="phone2" label='Secondary Phone' />
-                    <TextField sx={{ width: { md: '100%', lg: '100%' }}} name="email" label='Email' />
                     <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="location" label='Location/Apartment' required />
                     <TextField sx={{ width: { md: '48%', lg: '48%' }}} name="ip" label='IP Address' required />
                     <Select
