8da0cfeb41a7350a62fde42cc1fef9a18d964132
elsafrica <151996828+elsafrica@users.noreply.github.com>
2023-12-15 17:39:35 +0300

    Allow insecure requests from remote api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      </head>
       <ContextProvider>
         <QueryProvider>
           <Themify>
