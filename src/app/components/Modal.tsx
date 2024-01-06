b4863a4e8209a68f1bb4e1d2da9079f67919daa1
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-06 19:02:50 +0300

    Add a new prop to enable modal sizing

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
+import { SxProps } from '@mui/material';
 
 const Modal = ({
   children,
   open,
   title,
+  fullWidth = false,
+  maxWidth = 'sm',
   onClose,
 } : {
   children: React.ReactNode,
   open: boolean,
   title: string,
+  fullWidth?: boolean,
+  maxWidth?: 'sm' | 'md' | 'lg' | 'xl',
   onClose: () => void,
 }) => {
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog open={open} onClose={onClose} fullWidth={fullWidth} maxWidth={maxWidth}>
       <DialogTitle>{title}</DialogTitle>
-      <DialogContent>
-        {children}
-      </DialogContent>
+      {children}
     </Dialog>
   )
 }
